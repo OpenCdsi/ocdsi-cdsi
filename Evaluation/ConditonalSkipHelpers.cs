4f785688d298aaf30735898347baa98bba04fdb0
Dennis Dunn <dennisdunn@users.noreply.github.com>
2022-12-02 12:27:17 -0500

    Implemented control flow for ConditionalSkip()

---
+++ Evaluation/ConditonalSkipHelpers.cs
@@ -0,0 +1,29 @@
+﻿using OpenCdsi.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCdsi.Cdsi
+{
+    public static class ConditonalSkipHelpers
+    {
+        public static bool Evaluate(this antigenSupportingDataSeriesSeriesDoseConditionalSkip thing, IEvaluationOptions opts, IDoseEvaluator doses)
+        {
+            return thing.setLogic == "AND"
+                 ? thing.set.All(x => x.Evaluate(opts, doses))
+                 : thing.set.Any(x => x.Evaluate(opts, doses));
+        }
+        public static bool Evaluate(this antigenSupportingDataSeriesSeriesDoseConditionalSkipSet thing, IEvaluationOptions opts, IDoseEvaluator doses)
+        {
+            return thing.conditionLogic == "AND"
+                 ? thing.condition.All(x => x.Evaluate(opts, doses))
+                 : thing.condition.Any(x => x.Evaluate(opts, doses));
+        }
+        public static bool Evaluate(this antigenSupportingDataSeriesSeriesDoseConditionalSkipSetCondition thing, IEvaluationOptions opts, IDoseEvaluator doses)
+        {
+            return true;
+        }
+    }
+}
